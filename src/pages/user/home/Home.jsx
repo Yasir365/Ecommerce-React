89128ac52e8fa0c17bd893d15f9d85569bc5da57
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-09-23 17:12:39 +0500

    Update Home.jsx

--- src/pages/user/home/Home.jsx
+++ src/pages/user/home/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
     useEffect(() => {
         fetchProducts(1, 4, '', '', setFeaturedProducts);
         fetchProducts(2, 4, '', '', setDealsProducts);
-        fetchProducts(undefined, undefined, '', '66f14d8d8c7e664f1c97bb37', setBlogData);
+        fetchProducts(undefined, undefined, '', '66f1597c94dfa84f0701d9a3', setBlogData);
     }, []);
 
     const fetchProducts = async (page, itemsPerPage, search = '', productId = '', setState) => {
