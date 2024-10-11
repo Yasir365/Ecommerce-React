363097710edd3b7d8d04c9c35fded42b5a9914d8
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-10-11 15:02:21 +0500

    updated

--- src/components/out-team/Our-Team.jsx
+++ src/components/out-team/Our-Team.jsx
@@ -1,97 +1,93 @@
 import './our-Team.scss';
-import OwlCarousel from 'react-owl-carousel';
+import { Swiper, SwiperSlide } from "swiper/react";
+import next from "/images/next.svg";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { EffectCoverflow, Navigation, Pagination } from 'swiper/modules';
 
 const OurTeam = () => {
-    const options = {
-        loop: true,
-        margin: 10,
-        nav: true,
-        autoplay: false,
-        autoplayTimeout: 3000,
-        autoplayHoverPause: true,
-        smartSpeed: 600,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 3,
-            },
-            1000: {
-                items: 5,
-            },
+    const teamData = [
+        {
+            image: "/images/member/1.webp",
+            name: "John Doe",
+            designation: "CEO",
         },
-        navText: ["<", ">"],
-        slideBy: 1,
-    };
-
+        {
+            image: "/images/member/2.webp",
+            name: "Jane ",
+            designation: "CTO",
+        },
+        {
+            image: "/images/member/3.webp",
+            name: "Michael",
+            designation: "COO",
+        },
+        {
+            image: "/images/member/4.webp",
+            name: "Emily ",
+            designation: "HOD",
+        },
+        {
+            image: "/images/member/1.webp",
+            name: "John Doe",
+            designation: "CEO",
+        }
+    ]
+    const middle = Math.floor(teamData.length / 2);
     return (
-        <div className="team-carousel-section">
-            <h2 className='heading'>Our Team</h2>
-            <OwlCarousel className="owl-theme" {...options}>
-                <div className="item">
-                    <img src='/images/member/1.webp' alt="Team Member" className='test' />
-                    <h3>John Doe</h3>
-                    <p>CEO</p>
-                    <div className="social">
-                        <i className="fa-solid fa-x"></i>
-                        <i className="fa-brands fa-instagram"></i>
-                        <i className="fa-brands fa-facebook"></i>
-                    </div>
-                </div>
-                <div className="item">
-                    <img src='/images/member/2.webp' alt="Team Member" className='test' />
-                    <h3>Jane </h3>
-                    <p>CTO</p>
-                    <div className="social">
-                        <i className="fa-solid fa-x"></i>
-                        <i className="fa-brands fa-instagram"></i>
-                        <i className="fa-brands fa-facebook"></i>
-                    </div>
-                </div>
-                <div className="item">
-                    <img src='/images/member/3.webp' alt="Team Member" className='test' />
-                    <h3>Michael </h3>
-                    <p>COO</p>
-                    <div className="social">
-                        <i className="fa-solid fa-x"></i>
-                        <i className="fa-brands fa-instagram"></i>
-                        <i className="fa-brands fa-facebook"></i>
-                    </div>
-                </div>
-                <div className="item">
-                    <img src='/images/member/4.webp' alt="Team Member" className='test' />
-                    <h3>Emily </h3>
-                    <p>HOD</p>
-                    <div className="social">
-                        <i className="fa-solid fa-x"></i>
-                        <i className="fa-brands fa-instagram"></i>
-                        <i className="fa-brands fa-facebook"></i>
-                    </div>
-                </div>
-                <div className="item">
-                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj8sKRgBGHeqyyzcVzby3YrHH_s0KVk-PozzvgrCdsueqkbhorjmZ0cByvks-Oy9tK38M&usqp=CAU' alt="Team Member" className='test' />
-                    <h3>Emily </h3>
-                    <p>HOD</p>
-                    <div className="social">
-                        <i className="fa-solid fa-x"></i>
-                        <i className="fa-brands fa-instagram"></i>
-                        <i className="fa-brands fa-facebook"></i>
-                    </div>
-                </div>
-                <div className="item">
-                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbCFD9hBq5ZBfdDqHa1IPFZORSL3EkPSxU2tomxsaeiOcuOyQMbUhNN-htl5xLTtZwvMU&usqp=CAU' alt="Team Member" className='test' />
-                    <h3>Emily </h3>
-                    <p>HOD</p>
-                    <div className="social">
-                        <i className="fa-solid fa-x"></i>
-                        <i className="fa-brands fa-instagram"></i>
-                        <i className="fa-brands fa-facebook"></i>
-                    </div>
-                </div>
-            </OwlCarousel>
+        <div className="container team-carousel-section mx-auto">
+            <div style={{ overflow: "hidden", width: "100%", padding: "0 15px" }}>
+                <Swiper
+                    effect={"coverflow"}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    initialSlide={0}
+                    slidesPerView={5}
+                    spaceBetween={30}
+                    autoplay={{
+                        delay: 500,
+                        disableOnInteraction: false,
+                    }}
+                    breakpoints={{
+                        320: { slidesPerView: 1 },
+                        768: { slidesPerView: 2 },
+                        1024: { slidesPerView: 3 },
+                    }}
+                    coverflowEffect={{
+                        rotate: 0,
+                        stretch: 0,
+                        depth: 120,
+                        modifier: 6,
+                        slideShadows: false,
+                    }}
+                    pagination={true}
+                    navigation={true}
+                    modules={[EffectCoverflow, Pagination, Navigation]}
+                    className="mySwiper"
+                >
+                    {teamData?.map((item, index) => (
+                        <SwiperSlide
+                            key={index}
+                            style={{
+                                border: "2px solid #D5D2D1",
+                                borderRadius: "20px",
+                                maxWidth: "333px",
+                                height: "100%",
+                                width: "100%",
+                            }}
+                        >
+                            <img src={item.image} />
+                            <div className='ps-3 pt-3 pb-4'>
+                                <div> {item.name} </div>
+                                <div> {item.designation} </div>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
         </div>
     );
-};
-
+}
 export default OurTeam;
